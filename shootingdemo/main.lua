cb7002230ce3580180873fae9c7facbbade80095
fisherY9 <2963193623@qq.com>
2017-04-05 21:37:03 +0800

    a demo

---
+++ shootingdemo/main.lua
@@ -0,0 +1,23 @@
+--shooting game demo
+
+function love.load()
+	character = {
+		x = 0,
+		y = 0,
+		w = 0,
+		h = 0,
+		cdspace = 0,
+		cdspell = 0,
+		l = 0,
+		r = 0,
+		t = 0,
+		b = 0,
+		hp = 100,
+		vx = 0,
+		vy = 0,
+		ax = 0,
+		ay = 0
+	} 
+
+		
+end 
